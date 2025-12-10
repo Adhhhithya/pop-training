8c0c767110b904f5de4ff82dd46c6a5b1989ce56
Adhhhithya <adhithyaj06@gmail.com>
2025-12-10 09:14:29 +0530

    Java Collections

---
+++ day-4/collections_usage.java
@@ -0,0 +1,37 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+import java.util.Vector;
+public class collections_usage {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> arr = new ArrayList<>();
+        LinkedList<String> ll = new LinkedList<>();
+        Vector<Integer> vec = new Vector<>();
+        Stack<Integer> st = new Stack<>();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Queue<String> queue_ll = new LinkedList<>();
+        Deque<Integer> deque_arr = new ArrayDeque<>();
+        Deque<String> deque_ll = new LinkedList<>();
+
+        Set<Integer> hash_set = new HashSet<>();
+        Set<String> linked_hash_set = new LinkedHashSet<>();
+        SortedSet<Integer> tree_set = new TreeSet<>();
+
+
+
+
+    }
+    
+}
