9fcb020700b0204597c5b1b13267e853d6ad1f91
Adhhhithya <adhithyaj06@gmail.com>
2025-12-12 14:35:35 +0530

    Binary Codes of Numbers in a range

--- day-5/binary_code_generation.java
+++ day-5/binary_code_generation.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class binary_code_generation{
   public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    int x=1;
-    int n=sc.nextInt();
+    try (Scanner sc = new Scanner(System.in)) {
+      int x=1;
+      int n=sc.nextInt();
     for(int i=0;i<n;i++){
       if(i<n-1){
       System.out.print(Integer.toBinaryString(x)+", ");
@@ -12,7 +12,7 @@ public class binary_code_generation{
         System.out.print(Integer.toBinaryString(x));
       }
       x=x+1;
+      }
     }
-    sc.close();
   }
 }
